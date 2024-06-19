8fa16c6ce7175c39c70609f90ca0c941b7859a2b
welch <welch_cn@163.com>
2024-06-19 21:44:37 +0800

    update

--- main.py
+++ main.py
@@ -14,22 +14,6 @@ class GPTConfig:
     n_head: int = 12
     n_embd: int = 768
     
-
-class block(nn.Module):
-    def __init__(self, config:GPTConfig) -> None:
-        super().__init__()
-        self.config = config
-
-        self.ln_1 = nn.LayerNorm(self.config.n_embd)
-        self.att = CasualSelfAttention(self.config)
-        self.ln_2 = nn.LayerNorm(self.config.n_embd)
-        self.mlp = MLP(self.config)
-    
-    def forward(self,x):
-        x = x + self.att(self.ln_1(x))
-        x = x + self.mlp(self.ln_2(x))
-        return x
-
 class CasualSelfAttention(nn.Module):
     def __init__(self, config) -> None:
         super().__init__()
@@ -37,10 +21,6 @@ class CasualSelfAttention(nn.Module):
         assert self.config.n_embd % self.config.n_head == 0
         self.c_attn = nn.Linear(self.config.n_embd, 3*self.config.n_embd)
         self.c_proj = nn.Linear(self.config.n_embd, self.config.n_embd)
-        self.register_buffer(
-            "bias", 
-            torch.tril(torch.ones(self.config.block_size, self.config.block_size)).view(1,1, self.config.block_size,self.config.block_size)
-        )
     
     def forward(self, x):
         # batch_size, Sequence length, embedding_size
@@ -53,14 +33,16 @@ class CasualSelfAttention(nn.Module):
         q = q.view(B, T, self.config.n_head, C//self.config.n_head).transpose(1,2)
         k = k.view(B, T, self.config.n_head, C//self.config.n_head).transpose(1,2)
         v = v.view(B, T, self.config.n_head, C//self.config.n_head).transpose(1,2)
-        # attention
-        attn = (q @ k.transpose(-2, -1))*(1.0/math.sqrt(q.shape[-1]))
-        # mask previous tokens
-        attn = attn.masked_fill(self.bias[:,:,T,T]==0, float("-inf"))
-        # cal attn score 
-        attn = F.softmax(attn, dim=-1)
-        # cal output
-        y = attn @ v
+        # # attention
+        # attn = (q @ k.transpose(-2, -1))*(1.0/math.sqrt(q.shape[-1]))
+        # # mask previous tokens
+        # attn = attn.masked_fill(self.bias[:,:,T,T]==0, float("-inf"))
+        # # cal attn score 
+        # attn = F.softmax(attn, dim=-1)
+        # # cal output
+        # y = attn @ v
+        # 上面计算attention的代码等价于scaled_dot_product_attention函数
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True) # flash attention
         y = y.transpose(1,2).contiguous().view(B,T,C)
         y = self.c_proj(y)
         return y
@@ -80,7 +62,23 @@ class MLP(nn.Module):
         x = self.c_proj(x)
         return x
 
+class block(nn.Module):
+    def __init__(self, config:GPTConfig) -> None:
+        super().__init__()
+        self.config = config
+
+        self.ln_1 = nn.LayerNorm(self.config.n_embd)
+        self.attn = CasualSelfAttention(self.config)
+        self.ln_2 = nn.LayerNorm(self.config.n_embd)
+        self.mlp = MLP(self.config)
+    
+    def forward(self,x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
 class GPT(nn.Module):
+    
     def __init__(self, config:GPTConfig):
         super().__init__()
         self.config = config
@@ -108,11 +106,9 @@ class GPT(nn.Module):
         
         
     @classmethod
-    def from_pretrained(cls, model_type, override_args=None):
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingface"""
         assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
-        override_args = override_args or {} # default to empty dict
-        # only dropout can be overridden see more notes below
-        assert all(k == 'dropout' for k in override_args)
         from transformers import GPT2LMHeadModel
         print("loading weights from pretrained gpt: %s" % model_type)
 
@@ -123,7 +119,6 @@ class GPT(nn.Module):
             'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
             'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
         }[model_type]
-        print("forcing vocab_size=50257, block_size=1024, bias=True")
         config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
         config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
         # create a from-scratch initialized minGPT model
@@ -136,7 +131,7 @@ class GPT(nn.Module):
         # init a huggingface/transformers model
         model_hf = GPT2LMHeadModel.from_pretrained(model_type)
         sd_hf = model_hf.state_dict()
-        
+
         # copy while ensuring all of the parameters are aligned and match in names and shapes
         sd_keys_hf = sd_hf.keys()
         sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
@@ -160,10 +155,10 @@ class GPT(nn.Module):
         return model
 
 
-if __name__=="__main__":
-    model = GPT.from_pretrained("gpt2")
-    # model.eval()
-    # model.to("cuda")
-         
+
+model = GPT.from_pretrained("gpt2")
+model.eval()
+model.to("cuda")
+        
 
 
