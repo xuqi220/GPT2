6f9ed80185b364bab6c35f331823629043f06cef
welch <welch_cn@163.com>
2024-11-03 20:20:39 +0800

    update

--- gpt2_single_gpu.py
+++ gpt2_single_gpu.py
@@ -247,8 +247,6 @@ if __name__=="__main__":
     # 检测可用设备
     if torch.cuda.is_available():  
         device = "cuda" 
-    elif torch.backends.mps.is_available():
-        device = "mps"
     else:
         device = "cpu"
     
